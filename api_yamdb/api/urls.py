a31f6baed2ca794ec8774d031878419e671e73df
Krylov Dmitriy <dimakrylov2001@yandex.ru>
2024-03-07 17:53:23 +0300

    add router version

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,11 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
 
-router = DefaultRouter()
-router.register('categories', CategoriesViewSet, basename='categories')
-router.register('genres', GenresViewSet, basename='genres')
-router.register('titles', TitlesViewSet, basename='titles')
+router_v1 = DefaultRouter()
+router_v1.register('categories', CategoriesViewSet, basename='categories')
+router_v1.register('genres', GenresViewSet, basename='genres')
+router_v1.register('titles', TitlesViewSet, basename='titles')
 
 urlpatterns = [
-    path('v1/', include(router.urls))
+    path('v1/', include(router_v1.urls))
 ]
