f3cdc3a9dd3c463331e00209c1c7e4f1557afdc2
KrylovDmitrii <99659642+KrylovDmitrii@users.noreply.github.com>
2024-03-07 19:19:48 +0300

    Приложения, Роутинг и макеты под модельки и вьюхи (#1)
    * add projects apps and create layouts for my part
    
    * add router version
    
    * change way to define installed apps

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register('categories', CategoriesViewSet, basename='categories')
+router_v1.register('genres', GenresViewSet, basename='genres')
+router_v1.register('titles', TitlesViewSet, basename='titles')
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls))
+]
