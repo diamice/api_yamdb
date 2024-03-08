76d0f04115c826d26a3e7f2a86af9904f1a7107a
Анастасия Оксенчук <nastyaneplaksa@yandex.ru>
2024-03-08 14:00:31 +0300

    review permission

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
+
+class IsAuthorAdminModeratorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+    
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.role == User.MODERATOR
+            or request.user.role == User.ADMIN
+        )
