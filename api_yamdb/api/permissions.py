2b1250a8fb0a0feaf9453c18970a5a74f981cd45
Григорий Чезыбаев <gr_chez@mail.ru>
2024-03-15 22:50:34 +0700

    Merge with develop

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -54,29 +54,3 @@ class ReadOrAdminOnly(permissions.BasePermission):
                          or request.user.is_superuser)
                     )
                 )
-from rest_framework import permissions
-
-
-class IsAdmin(permissions.BasePermission):
-    """
-    Предоставляет доступ только администраторам.
-    """
-
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (request.user.is_staff
-                 or request.user.role == 'admin')
-        )
-
-
-class IsUAuthenticatedAndPatchMethod(permissions.BasePermission):
-    """
-    Предоставляет доступ аутенитифированным пользователям на чтение 
-    и на изменение своих пользовательских данных.
-    """
-    def has_permission(self, request, view):
-        return (
-            (request.method == 'patch' or request.method == 'get')
-            and request.user.is_authenticated
-        )
